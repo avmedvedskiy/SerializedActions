7a4ffb302895d90847b37a9823b644a5eceef2ac
avmedvedskiy <a.v.medvedskiy@gmail.com>
2022-09-19 15:39:35 +0300

    add extensions

---
+++ Runtime/Actions/ListActionExtention.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Actions
+{
+    public static class ListActionExtenstion
+    {
+        public static ActionRunner Run<T>(this List<T> actions) where T : class, IAction
+        {
+            ActionRunner runner = new ActionRunner();
+            runner.RunAsync(actions);
+            return runner;
+        }
+
+        public static void Run<T>(this List<T> actions, ActionRunner runner) where T : class, IAction
+        {
+            runner.RunAsync(actions).Forget();
+        }
+
+        public static async UniTask RunAsync<T>(this List<T> actions, ActionRunner runner) where T : class, IAction
+        {
+            await runner.RunAsync(actions);
+        }
+
+        public static async UniTask RunAsync<T>(this List<T> actions) where T : class, IAction
+        {
+            ActionRunner runner = new ActionRunner();
+            await runner.RunAsync(actions);
+        }
+    }
+}
+
